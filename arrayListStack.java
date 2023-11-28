42ab5a7c102437609fe084862f1932e94d3482f6
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-28 18:30:34 +0530

    Create arrayListStack.java
    Making stack using ArrayList.

---
+++ arrayListStack.java
@@ -0,0 +1,35 @@
+public class file4 {
+    static class Stack{
+        static ArrayList<Integer> list=new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size()==0;
+        }
+        public static void push(int data){
+            list.add(data);
+        }
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int n=list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return n;
+        }
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
